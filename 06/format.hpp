e0c2d4e8fe4358e9e69a43ca6099d10a20fc4f25
Bogdan <bogdan_aleksandrov_2014@mail.ru>
2021-06-04 14:02:06 +0300

    2 attempt

--- 06/format.hpp
+++ 06/format.hpp
@@ -6,7 +6,8 @@
 class brackets : public std::exception {};
 class no_number : public std::exception {};
 class wrong_number : public std::exception {};
-class no_arg : public std::exception {};
+class no_such_arg : public std::exception {};
+class unused_args : public std::exception {};
 
 template <class T>
 std::string convert_to_string(T&& arg) {
@@ -19,11 +20,13 @@ template <class... Args>
 std::string format(const std::string& str, Args&&... arg) {
     std::string new_str = "";
     std::vector<std::string> args = {convert_to_string(arg) ...};
+    std::vector<size_t> use_count;
+    use_count.resize(args.size(), 0);
     bool bracket_open = false;
     std::string num = "";
 
     std::string::const_iterator it = str.begin();
-    for(; it < str.end(); ++it) {
+    for (; it < str.end(); ++it) {
         if (bracket_open) {
 
             if (*it == '}') {
@@ -31,15 +34,23 @@ std::string format(const std::string& str, Args&&... arg) {
                 if (num.empty())
                     throw no_number();
 
-                size_t tmp = stoull(num);
+                size_t tmp;
+                try {
+                    tmp = stoull(num);
+                } catch(std::exception&) {
+                    throw wrong_number();
+                }
                 if (tmp >= args.size())
-                    throw no_arg();
+                    throw no_such_arg();
 
                 new_str += args[tmp];
+                ++use_count[tmp];
                 num.clear();
                 bracket_open = false;
 
             } else {
+                if (*it == '{')
+                    throw brackets();
 
                 if (!isdigit(*it))
                     throw wrong_number();
@@ -60,5 +71,10 @@ std::string format(const std::string& str, Args&&... arg) {
 
     if (bracket_open)
         throw brackets();
+    
+    for (auto it = use_count.begin(); it < use_count.end(); ++it) {
+        if (*it == 0)
+            throw unused_args();
+    }
     return new_str;
 }
--- 06/test.cpp
+++ 06/test.cpp
@@ -9,20 +9,11 @@ void TestCorrectness() {
     text = format("no brackets");
     assert(text == "no brackets");
 
-    text = format("no brackets 2 part", 23, "text", 0.5);
-    assert(text == "no brackets 2 part");
-
-    text = format("{0}{1}{2}", 1,2,3);
+    text = format("{0}{1}{2}", 1, 2, 3);
     assert(text == "123");
 
-    text = format("{4} is {3} {0} {2} {1}", "language", "world", "in the", "the best","C++");
+    text = format("{4} is {3} {0} {2} {1}", "language", "world", "in the", "the best", "C++");
     assert(text == "C++ is the best language in the world");
-
-    text = format("not {1}  {3} ", 0, "all", 0, "args");
-    assert(text == "not all  args ");
-
-    text = format("",1,"char");
-    assert(text == "");
 }
 
 void TestException() {
@@ -31,14 +22,14 @@ void TestException() {
     int key = 0;
     try {
         text = format("{0", "string");
-    } catch(brackets&) {
+    } catch(brackets& e) {
         ++key;
     }
     assert(key == 1);
 
     try {
         text = format("{{1}}", 0, 1);
-    } catch(wrong_number&) {
+    } catch(brackets&) {
         ++key;
     }
     assert(key == 2);
@@ -51,21 +42,158 @@ void TestException() {
     assert(key == 3);
 
     try {
-        text = format("{0}, {1}", 5);
-    } catch(no_arg&) {
+        text = format("My name is {}", "Steve");
+    } catch (no_number&) {
+        ++key;
+    }
+    assert(key == 4);
+}
+
+void OneArgTest() {
+    auto text = format("{0}", "only");
+    assert(text == "only");
+
+    int key = 0;
+    try {
+        text = format("{0}");
+    } catch(no_such_arg&) {
+        ++key;
+    }
+    assert(key == 1);
+
+    try {
+        text = format("{1}", 2);
+    } catch(no_such_arg&) {
+        ++key;
+    }
+    assert(key == 2);
+}
+
+void ErrorTest() {
+    auto text = format("{0}", 5);
+
+    int key = 0;
+    try {
+        text = format("{0}{", "string");
+    } catch(brackets&) {
+        ++key;
+    }
+    assert(key == 1);
+
+    try {
+       text = format("Just {0}}", "testing");
+    }
+    catch(brackets&){
+        ++key;
+    }
+    assert(key == 2);
+
+    try {
+        text = format("Some {{0} in {1}}", "words", "test");
+    } catch(brackets&) {
+        ++key;
+    }
+    assert(key == 3);
+
+    try {
+        text = format("Some {0}} word", "test");
+    } catch(brackets&) {
+        ++key;
+    }
+    assert(key == 4);
+}
+
+void WrongNumInBrackets() {
+    std::string text = "";
+
+    int key = 0;
+    try {
+        text = format("{}");
+    } catch(no_number&) {
+        ++key;
+    }
+    assert(key == 1);
+
+    try {
+        text = format("{{}}{}");
+    } catch(brackets&) {
+        ++key;
+    }
+    assert(key == 2);
+
+    try {
+        text = format("{string}", "string");
+    } catch(wrong_number&) {
+        ++key;
+    }
+    assert(key == 3);
+
+    try {
+        text = format("Text {0str} !", 0);
+    } catch(wrong_number&) {
         ++key;
     }
     assert(key == 4);
 
     try {
-        text = format("My name is {}", "Steve");
-    } catch (no_number&) {
+        text = format("{f1i0}", "text");
+    } catch(wrong_number&) {
         ++key;
     }
     assert(key == 5);
+
+    try {
+        text = format("{18446744073709551616}", "big num");
+    } catch(wrong_number&) {
+        ++key;
+    }
+    assert(key == 6);
+
+    try {
+        text = format("{18446744073709551615}", "big num");
+    } catch(no_such_arg&) {
+        ++key;
+    }
+    assert(key == 7);
+}
+
+void WrongArgs() {
+    std::string text;
+    int key = 0;
+    try {
+        text = format("Unused {1}", 1, "args");
+    } catch(unused_args&) {
+        ++key;
+    }
+    assert(key == 1);
+
+    try {
+        text = format("no brackets 2 part", 23, "text", 0.5);
+    } catch(unused_args&) {
+        ++key;
+    }
+    assert(key == 2);
+
+    try {
+        text = format("{0}, {1}", 5);
+    } catch(no_such_arg&) {
+        ++key;
+    }
+    assert(key == 3);
+
+    try {
+        text = format("", 1, "char");
+    } catch(unused_args&) {
+        ++key;
+    }
+    assert(key == 4);
 }
 
 int main() {
     TestCorrectness();
     TestException();
+    OneArgTest();
+    ErrorTest();
+    WrongNumInBrackets();
+    WrongArgs();
 }
